3ee0aafea5e37472b908ed0fc41481dc8118add7
Manu Herrán <139716025+manuherran@users.noreply.github.com>
2024-07-08 10:52:28 +0200

    Add files via upload

---
+++ zip_0001.inc.php
@@ -0,0 +1,28 @@
+<?php
+// ----------------------------------------------------------------------
+// phpMyLib Version: 0.1.3 Release Date: 20140522
+// © Copyright 2001-2014 phpMyLib. Todos los derechos reservados
+// Free download source code:
+// http://phpMyLib.com/
+// ----------------------------------------------------------------------
+// Para mantener la compatibilidad entre editores de texto, en este 
+// fichero se utilizará para identar el espacio, y no el tabulador
+// ----------------------------------------------------------------------
+// zip_0001_fUnzipFileInFolder
+// 
+// 
+// ----------------------------------------------------------------------
+function zip_0001_fUnzipFileInFolder ($fileName, $folderName, $overwrite) {
+  $zip = new ZipArchive;
+  if ($zip->open($fileName) === TRUE) {
+    $zip->extractTo($folderName);
+    $zip->close();
+    return 1;
+  } else {
+    return 0;
+  }
+}
+// ----------------------------------------------------------------------
+// OJO NO DEJAR LINEAS EN BLANCO AL FINAL DE ESTE FICHERO
+// ----------------------------------------------------------------------
+?>
